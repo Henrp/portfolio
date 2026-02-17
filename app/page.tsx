88ec1829c015493ae2932a457c39f36ee319e85f
Henry Kim <henry1112200@gmail.com>
2026-02-17 13:59:07 -0800

    more content

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,34 @@ import "./globals.css";
 
 export default function Home() {
   return (
-    <div>
-      <div>henrp(at)berkeley(dot)edu</div>
-      <div>
-        <header>Introduction</header>
-        <p>
-          Hello, my name is Kemal — an earth based design engineer. I build
-          things on (and for) the web. For a majority of my career, I have
-          shape-shifted between product design and frontend engineering.
-        </p>
+    <div className="max-w-2xl mx-auto">
+      <div className="py-10 text-primary">
+        henrp<span className="text-secondary">(at)</span>berkeley
+        <span className="text-secondary">(dot)</span>edu
       </div>
+      <div className="space-y-6">
+        <div>
+          <header>Introduction</header>
+          <p className="text-primary pl-4 leading-6">
+            Hi 👋 I'm Henry. <br />
+            I'm a computing and music enthusiast majoring in Electrical
+            Engineering and Computer Sciences (EECS) at UC Berkeley. I'm
+            passionate about the intersection of AI, music, and emotions, and I
+            build tools that help humans connect with sound and feeling through
+            affective computing, machine learning, and app development.
+          </p>
+        </div>
 
-      <div>The Present</div>
-      <div>Experience</div>
-      <div>Get in touch</div>
+        <div>
+          <header>The Present</header>
+          <p className="text-primary pl-4 leading-6">
+            Software Engineer at{" "}
+            <span className="text-accent">Berkeleytime</span>
+          </p>
+        </div>
+        <div>Experience</div>
+        <div>Get in touch</div>
+      </div>
       <div>Explore</div>
     </div>
   );
